f0c41e7eba65c5e7d30998a8e45955359705fa16
Anatoli <anatoli@anatoli.home>
2024-02-15 19:40:23 +0300

    Feb 15 Work

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Navbar from "../Navbar";
+
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <Navbar />
+
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-6">
+            <div id="map"></div>
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">Contact Us</h1>
+            <p className="lead ">
+              "Reach out to us today! Use our contact form to get in touch."
+            </p>
+
+            <form className="p-3 p-md-5 border rounded-3 bg-light">
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                />
+                <label htmlFor="floatingInput">Email address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <textarea
+                  className="form-control"
+                  rows={4}
+                  id="floatingPassword"
+                ></textarea>
+                <label htmlFor="floatingPassword">Enter Message</label>
+              </div>
+              <div className="checkbox mb-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Remember me
+                </label>
+              </div>
+              <button className="w-100 btn btn-lg btn-primary" type="submit">
+                Submit
+              </button>
+              <hr className="my-4" />
+              <small className="text-muted">
+                By clicking Sent, you agree to the terms of use.
+              </small>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact
--- src/components/pages/Products.tsx
+++ src/components/pages/Products.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
-
+import Navbar from '../Navbar';
 function Products() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Navbar />
+      <hr />
+      <hr />
+      <div className="row row-cols-1 row-cols-md-3 g-4 px-2 py-4">
+        <div className="col  px-2">
+          <div className="border border-success border-4">
+            <div className="card h-50 ">
+              <img
+                src="https://images.unsplash.com/photo-1558818498-28c1e002b655?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="card-img-top"
+                alt="Tomatoes"
+              />
+              <div className="card-body">
+                <h5 className="card-title badge bg-success fs-6">Tomatoes</h5>
+                <h6 className="fs-7 fw-bolder">
+                  Price:<i>Ksh.50/=</i>
+                </h6>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                  natoque penatibus et magnis dis parturient montes, nascetur
+                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                  eu, pretium quis, sem. Nulla consequat massa.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col px-2">
+          <div className="border border-success border-4">
+            <div className="card h-50 ">
+              <img
+                src="https://images.unsplash.com/photo-1547514701-42782101795e?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title badge bg-success fs-6">Oranges</h5>
+                <h6 className="fs-7 fw-bolder">
+                  Price:<i>Ksh.60/=</i>
+                </h6>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                  natoque penatibus et magnis dis parturient montes, nascetur
+                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                  eu, pretium quis, sem. Nulla consequat massa
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col px-2">
+          <div className="border border-success border-4">
+            <div className="card h-40">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1674666545866-d02e25597939?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fGF2YWNhZG98ZW58MHx8MHx8fDA%3D"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title badge bg-success fs-6">Avocado</h5>
+                <h6 className="fs-7 fw-bolder">
+                  Price:<i>Ksh.80/=</i>
+                </h6>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+                  natoque penatibus et magnis dis parturient montes, nascetur
+                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+                  eu, pretium quis, sem. Nulla consequat massa
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Products
